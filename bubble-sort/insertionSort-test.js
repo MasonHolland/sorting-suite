efb060f01323357e147f07e925eb36f0a85e32cc
MasonHolland <masontholland@gmail.com>
2017-10-01 20:05:17 -0600

    complete insertionSort-test

---
+++ bubble-sort/insertionSort-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const insertionSort = require('./insertionSort')
+
+describe("insertionSort can sort an array", function() {
+  context("when given an unorganized array", function() {
+    it("returns it sorted", function() {
+      assert.deepEqual(insertionSort([5,2,1,3,4], [1,2,3,4,5]))
+    });
+  });
+});
